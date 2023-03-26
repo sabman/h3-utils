1205959531b86ad1e5a3d4f7d423891e0bbf8b86
Shoaib Burq <saburq@gmail.com>
2023-03-27 00:00:45 +0500

    move viz functions to utils

--- h3_utils/viz.py
+++ h3_utils/viz.py
@@ -1,4 +1,7 @@
-import folium, h3
+import folium
+import h3
+from h3_utils import tools as h3_tools
+
 
 def visualize_hexagons(hexagons, color="red", folium_map=None):
     """
@@ -13,25 +16,110 @@ def visualize_hexagons(hexagons, color="red", folium_map=None):
         # flatten polygons into loops.
         outlines = [loop for polygon in polygons for loop in polygon]
         polyline = [outline + [outline[0]] for outline in outlines][0]
-        lat.extend(map(lambda v:v[0],polyline))
-        lng.extend(map(lambda v:v[1],polyline))
+        lat.extend(map(lambda v: v[0], polyline))
+        lng.extend(map(lambda v: v[1], polyline))
         polylines.append(polyline)
-    
+
     if folium_map is None:
-        m = folium.Map(location=[sum(lat)/len(lat), sum(lng)/len(lng)], zoom_start=13, tiles='cartodbpositron')
+        m = folium.Map(location=[sum(lat)/len(lat), sum(lng) /
+                       len(lng)], zoom_start=13, tiles='cartodbpositron')
     else:
         m = folium_map
     for polyline in polylines:
-        my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
+        my_PolyLine = folium.PolyLine(
+            locations=polyline, weight=8, color=color)
         m.add_child(my_PolyLine)
     return m
-    
+
 
 def visualize_polygon(polyline, color):
     polyline.append(polyline[0])
     lat = [p[0] for p in polyline]
     lng = [p[1] for p in polyline]
-    m = folium.Map(location=[sum(lat)/len(lat), sum(lng)/len(lng)], zoom_start=13, tiles='cartodbpositron')
-    my_PolyLine=folium.PolyLine(locations=polyline,weight=8,color=color)
+    m = folium.Map(location=[sum(lat)/len(lat), sum(lng) /
+                   len(lng)], zoom_start=13, tiles='cartodbpositron')
+    my_PolyLine = folium.PolyLine(locations=polyline, weight=8, color=color)
     m.add_child(my_PolyLine)
+    return m
+
+
+def create_hexagons_map(geojson, resolution=6):
+    # Extract the coordinates from the GeoJSON feature and calculate the average center
+    polyline = geojson['coordinates'][0]
+    polyline = list(map(lambda v: [v[1], v[0]], polyline))
+    polyline.append(polyline[0])
+    lat = [p[0] for p in polyline]
+    lng = [p[1] for p in polyline]
+    avg_center = [sum(lat)/len(lat), sum(lng)/len(lng)]
+
+    # Create a map centered on the feature
+    m = folium.Map(location=avg_center, zoom_start=13, tiles='cartodbpositron')
+
+    # Draw the original PolyLine on the map
+    my_polyline = folium.PolyLine(locations=polyline, weight=8, color="green")
+    m.add_child(my_polyline)
+
+    # Find the hexagons that intersect with the GeoJSON feature
+    found_hexagons, level = h3_tools.find_cells_for_geojson(geojson, 6)
+
+    if level > 6:
+        hexagons = [h3.h3_to_parent(h, 6) for h in found_hexagons]
+    else:
+        hexagons = found_hexagons
+
+    # Draw the hexagons on the map
+    for hex in hexagons:
+        polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
+        outlines = [loop for polygon in polygons for loop in polygon]
+        polyline = [outline + [outline[0]] for outline in outlines][0]
+        my_polyline = folium.PolyLine(
+            locations=polyline, weight=8, color='red')
+        m.add_child(my_polyline)
+
+    # Return the map object
+    return m
+
+
+def create_map_with_hexagons(geojson, cells):
+    # extract polyline coordinates
+    polyline = geojson['coordinates'][0]
+    # reverse the coordinates to be [lat, lng] since geojson is [x, y] i.e [lng, lat] but folium likes [lat, lng]
+    polyline = list(map(lambda v: [v[1], v[0]], polyline))
+    polyline.append(polyline[0])
+    lat = [p[0] for p in polyline]
+    lng = [p[1] for p in polyline]
+    avg_center = [sum(lat)/len(lat), sum(lng)/len(lng)]
+
+    # create a map centered on the feature
+    m = folium.Map(
+        location=avg_center,
+        zoom_start=16.4,
+        tiles='cartodbpositron'
+    )
+
+    # add original polyline to the map
+    my_PolyLine = folium.PolyLine(
+        locations=polyline,
+        weight=4,
+        color="green"
+    )
+    m.add_child(my_PolyLine)
+
+    # add hexagon boundaries to the map
+    polylines = []
+    lat = []
+    lng = []
+    for hex in cells:
+        polygons = h3.h3_set_to_multi_polygon([hex], geo_json=False)
+        # flatten polygons into loops.
+        outlines = [loop for polygon in polygons for loop in polygon]
+        polyline = [outline + [outline[0]] for outline in outlines][0]
+        lat.extend(map(lambda v: v[0], polyline))
+        lng.extend(map(lambda v: v[1], polyline))
+        polylines.append(polyline)
+    for polyline in polylines:
+        my_PolyLine = folium.PolyLine(
+            locations=polyline, weight=4, color='red')
+        m.add_child(my_PolyLine)
+
     return m
